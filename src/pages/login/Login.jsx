45942b550ce93ed6b07a16ee3c6d9b6a238f74c6
AndoKami <hei.ando.1007@gmail.com>
2023-08-29 09:26:50 +0300

    feat: adding new values to the confirm pass

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.scss";
+import { api } from "./provider/api";
 
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+  const [username, setUsername] = useState();
+  const navigate = useNavigate()
+
+  let data = {
+    "email" :   email,
+    "password": password,
+    "confirmPassword": confirmPassword,
+    "username": username
+  }
+
+const login = async () => {
+  try {
+    const response = await api.post('/users', data);
+    console.log(response);
+    navigate("/")
+  } catch (error) {
+      console.log("ERROR: ", error.response.data.message);
+  }
+}
+
   return (
     <div className="login">
       <div className="loginWrapper">
@@ -15,12 +39,13 @@ const Login = () => {
         <div className="loginRight">
           <div className="loginBox">
             <div className="bottom">
-              <form className="bottomBox">
+              <form className="bottomBox" onSubmit={() => login(data)}>
                 <input
                   type="email"
                   placeholder="Email"
                   id="email"
                   className="loginInput"
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
                 <input
@@ -28,12 +53,30 @@ const Login = () => {
                   placeholder="Password"
                   id="password"
                   className="loginInput"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+                 <input
+                  type="password"
+                  placeholder="ConfirmPassword"
+                  id="password"
+                  className="loginInput"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+                 <input
+                  type="text"
+                  placeholder="Username"
+                  id="password"
+                  className="loginInput"
+                  onChange={(e) => setUsername(e.target.value)}
                   required
                 />
 
-                <button type="submit" className="loginButton">
+                <button type="submit" className="loginButton" onClick={() => login}>
                   Sign In
                 </button>
+
                 <Link to="/register">
                   <button className="loginRegisterButton">
                     Create a New Account
